9fb0d208b121f9df8e83f49a86b3bc46959d7a04
Jakub <jakubkonopka@student.agh.edu.pl>
2025-06-03 13:22:36 +0200

    Run ruff

--- segmentation/examples/train.py
+++ segmentation/examples/train.py
@@ -1,19 +1,22 @@
 import torch
+
 from segmentation.models.unet3d import UNet3D
-from segmentation.train import train, dice_loss
+from segmentation.train import dice_loss, train
+
 
 def example_train():
     model = UNet3D(
         in_channels=4,
         num_classes=4,
         level_channels=[16, 32, 64],
-        bottleneck_channels=128
+        bottleneck_channels=128,
     )
-    
+
     criterion = dice_loss
     optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3)
-    
+
     train(model, criterion, optimizer)
+
 
 if __name__ == "__main__":
     example_train()
