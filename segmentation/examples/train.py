439ed8ddbb21bf54d4d95c34be8a4fb4bcf5691d
Jakub <jakubkonopka@student.agh.edu.pl>
2025-06-06 16:34:29 +0200

    Bundle all training functions into one Trainer class

--- segmentation/examples/train.py
+++ segmentation/examples/train.py
@@ -1,10 +1,16 @@
 import torch
 
 from segmentation.models.unet3d import UNet3D
-from segmentation.train import dice_loss, train
+from segmentation.train import Trainer
+
+from pathlib import Path
 
 
 def example_train():
+    current_dir = Path(__file__).parent
+    project_root = current_dir.parent.parent
+    dataset_dir = project_root / "datasets" / "Task01_BrainTumour"
+
     model = UNet3D(
         in_channels=4,
         num_classes=4,
@@ -12,10 +18,21 @@ def example_train():
         bottleneck_channels=128,
     )
 
-    criterion = dice_loss
     optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3)
 
-    train(model, criterion, optimizer)
+    trainer = Trainer(
+        model=model,
+        optimizer=optimizer,
+    )
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    trainer.train(
+        dataset_dir=dataset_dir,
+        total_samples=10,
+        epochs=3,
+        device=device,
+    )
 
 
 if __name__ == "__main__":
--- segmentation/train.py
+++ segmentation/train.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from pathlib import Path
 import time
-from typing import Any, Callable, Dict, List, Tuple
+from typing import Any, Dict, List, Tuple
 
 import numpy as np
 import torch
@@ -11,211 +11,230 @@ from tqdm import tqdm
 from segmentation.dataset import BrainTumorDataset
 from segmentation.transforms import train_transforms
 
-CONFIG: Dict[str, Any] = {
-    "batch_size": 1,
-    "epochs": 5,
-    "val_split": 0.2,
-    "device": "cuda" if torch.cuda.is_available() else "cpu",
-    "save_dir": Path("checkpoints"),
-}
+
+class Trainer:
+    """
+    Trainer class for training and validating a brain tumor segmentation model.
+    """
+
+    def __init__(
+            self,
+            model: torch.nn.Module,
+            optimizer: torch.optim.Optimizer,
+    ) -> None:
+        self.data_config: Dict[str, Any] = {
+            "batch_size": 1,
+            "val_split": 0.2,
+            "save_dir": Path("checkpoints"),
+        }
+        self.model = model
+        self.optimizer = optimizer
+
+    def get_datasets(
+        self,
+        root_dir: Path,
+        total_samples: int,
+    ) -> Tuple[torch.utils.data.Subset, torch.utils.data.Subset]:
+        full_dataset = BrainTumorDataset(
+            root_dir=str(root_dir),
+            split="train",
+            modalities=["FLAIR", "T1w", "t1gd", "T2w"],
+            transform=train_transforms,
+            target_transform=train_transforms,
+            cache_data=False,
+        )
+
+        if total_samples > 0 and total_samples < len(full_dataset):
+            indices = torch.randperm(len(full_dataset))[:total_samples].tolist()
+            final_dataset = torch.utils.data.Subset(full_dataset, indices)
+        else:
+            final_dataset = full_dataset
+            total_samples = len(full_dataset)
+
+        val_size = int(self.data_config["val_split"] * total_samples)
+        train_size = total_samples - val_size
+
+        train_dataset, val_dataset = torch.utils.data.random_split(
+            final_dataset, [train_size, val_size]
+        )
+
+        print(f"Training samples: {train_size}, Validation samples: {val_size}")
+
+        return train_dataset, val_dataset
 
 
-def get_datasets(
-    root_dir: Path,
-) -> Tuple[torch.utils.data.Subset, torch.utils.data.Subset]:
-    full_dataset = BrainTumorDataset(
-        root_dir=str(root_dir),
-        split="train",
-        modalities=["FLAIR", "T1w", "t1gd", "T2w"],
-        transform=train_transforms,
-        target_transform=train_transforms,
-        cache_data=False,
-    )
+    def create_dataloaders(
+        self,
+        train_dataset: torch.utils.data.Subset,
+        val_dataset: torch.utils.data.Subset,
+    ) -> Tuple[DataLoader, DataLoader]:
+        train_loader = DataLoader(
+            train_dataset,
+            batch_size=self.data_config["batch_size"],
+            shuffle=True,
+        )
 
-    dataset_size = len(full_dataset)
-    val_size = int(CONFIG["val_split"] * dataset_size)
-    train_size = dataset_size - val_size
+        val_loader = DataLoader(
+            val_dataset,
+            batch_size=self.data_config["batch_size"],
+            shuffle=False,
+        )
 
-    train_dataset, val_dataset = torch.utils.data.random_split(
-        full_dataset, [train_size, val_size]
-    )
-
-    print(f"Training samples: {train_size}, Validation samples: {val_size}")
-
-    return torch.utils.data.Subset(train_dataset, [0, 1, 2]), torch.utils.data.Subset(
-        val_dataset, [0, 1]
-    )
-    # return train_dataset, val_dataset
+        return train_loader, val_loader
 
 
-def create_dataloaders(
-    train_dataset: torch.utils.data.Subset, val_dataset: torch.utils.data.Subset
-) -> Tuple[DataLoader, DataLoader]:
-    train_loader = DataLoader(
-        train_dataset,
-        batch_size=CONFIG["batch_size"],
-        shuffle=True,
-    )
+    def dice_loss(
+        self,
+        pred: torch.Tensor,
+        target: torch.Tensor,
+    ) -> torch.Tensor:
+        pred = pred.contiguous()
+        target = target.contiguous()
 
-    val_loader = DataLoader(
-        val_dataset,
-        batch_size=CONFIG["batch_size"],
-        shuffle=False,
-    )
+        # tensor shape: (batch_size, num_classes, depth, height, width)
+        intersection = (pred * target).sum(dim=(2, 3, 4))
+        loss = 1 - (
+            (2 * intersection) / (pred.sum(dim=(2, 3, 4)) + target.sum(dim=(2, 3, 4)))
+        )
 
-    return train_loader, val_loader
-
-
-def dice_loss(pred: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
-    pred = pred.contiguous()
-    target = target.contiguous()
-
-    # tensor shape: (batch_size, num_classes, depth, height, width)
-    intersection = (pred * target).sum(dim=(2, 3, 4))
-    loss = 1 - (
-        (2 * intersection) / (pred.sum(dim=(2, 3, 4)) + target.sum(dim=(2, 3, 4)))
-    )
-
-    return loss.mean()
+        return loss.mean()
 
 
-def dice_coefficient(
-    pred: torch.Tensor, target: torch.Tensor, threshold: float = 0.5
-) -> torch.Tensor:
-    pred = (pred > threshold).float()
-    intersection = (pred * target).sum()
+    def dice_coefficient(
+        self,
+        pred: torch.Tensor,
+        target: torch.Tensor,
+        threshold: float = 0.5,
+    ) -> torch.Tensor:
+        pred = (pred > threshold).float()
+        intersection = (pred * target).sum()
 
-    return (2 * intersection) / (pred.sum() + target.sum())
-
-
-def train_one_epoch(
-    model: torch.nn.Module,
-    dataloader: DataLoader,
-    optimizer: torch.optim.Optimizer,
-    criterion: Callable[[torch.Tensor, torch.Tensor], torch.Tensor],
-    device: str,
-) -> tuple[float, float]:
-    model.train()
-    epoch_loss: float = 0.0
-    dice_scores: List[float] = []
-
-    with tqdm(dataloader, desc="Training") as progress:
-        for _, (images, masks) in enumerate(progress):
-            images, masks = images.to(device), masks.to(device)
-
-            optimizer.zero_grad()
-
-            outputs = model(images)
-            loss = criterion(outputs, masks)
-            loss.backward()
-            optimizer.step()
-
-            with torch.no_grad():
-                dice = dice_coefficient(outputs, masks)
-                dice_scores.append(dice.item())
-                epoch_loss += loss.item()
-
-            if device == "cuda":
-                torch.cuda.empty_cache()
-
-    return epoch_loss / len(dataloader), float(np.mean(dice_scores))
+        return (2 * intersection) / (pred.sum() + target.sum())
 
 
-def validate(
-    model: torch.nn.Module,
-    dataloader: DataLoader,
-    criterion: Callable[[torch.Tensor, torch.Tensor], torch.Tensor],
-    device: str,
-) -> tuple[float, float]:
-    model.eval()
-    val_loss: float = 0.0
-    dice_scores: List[float] = []
+    def train_one_epoch(
+        self,
+        dataloader: DataLoader,
+        device: str,
+    ) -> tuple[float, float]:
+        self.model.train()
+        epoch_loss: float = 0.0
+        dice_scores: List[float] = []
 
-    with torch.no_grad():
-        with tqdm(dataloader, desc="Validation") as progress:
+        with tqdm(dataloader, desc="Training") as progress:
             for _, (images, masks) in enumerate(progress):
                 images, masks = images.to(device), masks.to(device)
 
-                outputs = model(images)
-                loss = criterion(outputs, masks)
+                self.optimizer.zero_grad()
 
-                dice = dice_coefficient(outputs, masks)
-                dice_scores.append(dice.item())
-                val_loss += loss.item()
+                outputs = self.model(images)
+                loss = self.dice_loss(outputs, masks)
+                loss.backward()
+                self.optimizer.step()
 
-    return val_loss / len(dataloader), float(np.mean(dice_scores))
+                with torch.no_grad():
+                    dice = self.dice_coefficient(outputs, masks)
+                    dice_scores.append(dice.item())
+                    epoch_loss += loss.item()
+
+                if device == "cuda":
+                    torch.cuda.empty_cache()
+
+        return epoch_loss / len(dataloader), float(np.mean(dice_scores))
 
 
-def train(
-    model: torch.nn.Module,
-    criterion: Callable[[torch.Tensor, torch.Tensor], torch.Tensor],
-    optimizer: torch.optim.Optimizer,
-) -> Dict[str, Any]:
-    current_dir = Path(__file__).parent
-    project_root = current_dir.parent
-    dataset_dir = project_root / "datasets" / "Task01_BrainTumour"
+    def validate(
+        self,
+        dataloader: DataLoader,
+        device: str,
+    ) -> tuple[float, float]:
+        self.model.eval()
+        val_loss: float = 0.0
+        dice_scores: List[float] = []
 
-    train_dataset, val_dataset = get_datasets(dataset_dir)
-    train_loader, val_loader = create_dataloaders(train_dataset, val_dataset)
+        with torch.no_grad():
+            with tqdm(dataloader, desc="Validation") as progress:
+                for _, (images, masks) in enumerate(progress):
+                    images, masks = images.to(device), masks.to(device)
 
-    device = CONFIG["device"]
-    model = model.to(device)
+                    outputs = self.model(images)
+                    loss = self.dice_loss(outputs, masks)
 
-    best_val_dice: float = 0.0
+                    dice = self.dice_coefficient(outputs, masks)
+                    dice_scores.append(dice.item())
+                    val_loss += loss.item()
 
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    save_dir = CONFIG["save_dir"] / f"run_{timestamp}"
-    save_dir.mkdir(exist_ok=True, parents=True)
+        return val_loss / len(dataloader), float(np.mean(dice_scores))
 
-    train_losses: List[float] = []
-    val_losses: List[float] = []
-    train_dices: List[float] = []
-    val_dices: List[float] = []
 
-    print(f"Training on device: {CONFIG['device']}")
+    def train(
+        self,
+        dataset_dir: Path,
+        total_samples: int = -1,
+        epochs: int = 5,
+        device: str = "cpu",
+    ) -> Dict[str, Any]:
+        train_dataset, val_dataset = self.get_datasets(dataset_dir, total_samples)
+        train_loader, val_loader = self.create_dataloaders(train_dataset, val_dataset)
 
-    start = time.time()
+        self.model = self.model.to(device)
 
-    for epoch in range(CONFIG["epochs"]):
-        print(f"\nEpoch {epoch + 1}/{CONFIG['epochs']}")
+        best_val_dice: float = 0.0
 
-        train_loss, train_dice = train_one_epoch(
-            model, train_loader, optimizer, criterion, device
-        )
-        train_losses.append(train_loss)
-        train_dices.append(train_dice)
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        save_dir = self.data_config["save_dir"] / f"run_{timestamp}"
+        save_dir.mkdir(exist_ok=True, parents=True)
 
-        val_loss, val_dice = validate(model, val_loader, criterion, device)
-        val_losses.append(val_loss)
-        val_dices.append(val_dice)
+        train_losses: List[float] = []
+        val_losses: List[float] = []
+        train_dices: List[float] = []
+        val_dices: List[float] = []
 
-        print(f"Train Loss: {train_loss:.4f}, Train Dice: {train_dice:.4f}")
-        print(f"Val Loss: {val_loss:.4f}, Val Dice: {val_dice:.4f}")
+        print(f"Training on device: {device}")
 
-        if val_dice > best_val_dice:
-            best_val_dice = val_dice
-            torch.save(
-                {
-                    "epoch": epoch,
-                    "model_state_dict": model.state_dict(),
-                    "optimizer_state_dict": optimizer.state_dict(),
-                    "val_dice": val_dice,
-                },
-                save_dir / "best_model.pth",
+        start = time.time()
+
+        for epoch in range(epochs):
+            print(f"\nEpoch {epoch + 1}/{epochs}")
+
+            train_loss, train_dice = self.train_one_epoch(
+                train_loader, device
             )
-            print(f"Saved best model with Dice score: {val_dice:.4f}")
+            train_losses.append(train_loss)
+            train_dices.append(train_dice)
 
-    train_time = time.time() - start
-    print(
-        f"Training completed in {train_time:.2f} seconds ({train_time / 60:.2f} minutes)"
-    )
+            val_loss, val_dice = self.validate(val_loader, device)
+            val_losses.append(val_loss)
+            val_dices.append(val_dice)
 
-    return {
-        "model": model,
-        "best_dice": best_val_dice,
-        "train_losses": train_losses,
-        "val_losses": val_losses,
-        "train_dices": train_dices,
-        "val_dices": val_dices,
-        "train_time": train_time,
-    }
+            print(f"Train Loss: {train_loss:.4f}, Train Dice: {train_dice:.4f}")
+            print(f"Val Loss: {val_loss:.4f}, Val Dice: {val_dice:.4f}")
+
+            if val_dice > best_val_dice:
+                best_val_dice = val_dice
+                torch.save(
+                    {
+                        "epoch": epoch,
+                        "model_state_dict": self.model.state_dict(),
+                        "optimizer_state_dict": self.optimizer.state_dict(),
+                        "val_dice": val_dice,
+                    },
+                    save_dir / "best_model.pth",
+                )
+                print(f"Saved best model with Dice score: {val_dice:.4f}")
+
+        train_time = time.time() - start
+
+        print(
+            f"Training completed in {train_time:.2f} seconds ({train_time / 60:.2f} minutes)"
+        )
+
+        return {
+            "model": self.model,
+            "best_dice": best_val_dice,
+            "train_losses": train_losses,
+            "val_losses": val_losses,
+            "train_dices": train_dices,
+            "val_dices": val_dices,
+            "train_time": train_time,
+        }
