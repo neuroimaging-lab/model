c82637ebddeeda1b2c2536c32173b1a4b44ee943
Mateusz Bobula <matbobula@student.agh.edu.pl>
2025-10-19 17:37:07 +0200

    typo

--- util/image_manipulation.py
+++ util/image_manipulation.py
@@ -5,7 +5,7 @@ def cut_images_and_masks(
     images: torch.Tensor, masks: torch.Tensor
 ) -> tuple[torch.Tensor, torch.Tensor]:
     """
-    Reduces voxel dimensions for the images and correspoding masks, by center-cropping.
+    Reduces voxel dimensions for the images and corresponding masks, by center-cropping.
     Used as a test tool for the local development - (e.g. 160x240x240 → 80x160x160) - to be able to fit an MRI image inside a model with full channels.
     """
     depth = 80  # base 160
