bcaa03081f0a22a9867e7d2a2c82b91f93481ac0
Norbert Dziwak <plgndziwak@login01.athena.cyfronet.pl>
2025-10-29 20:53:50 +0100

    Upgrade torch to 2.9.0

--- segmentation/config.py
+++ segmentation/config.py
@@ -4,6 +4,8 @@ from pathlib import Path
 
 CLUSTER_TRAINING_ENABLED = True  # NOTE: If False, 3D MRI images are center-cropped to fit into GPU memory, reducing their voxel dimensions (e.g. 160x240x240 → 80x160x160).
 
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
+
 
 def get_dataset_dir():
     scratch = os.environ.get(
